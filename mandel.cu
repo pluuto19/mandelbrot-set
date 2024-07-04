1c5f46c239fb2a44f1472169753825d2a57c1ee3
icantkode <ashersiddique127@gmail.com>
2024-07-05 02:21:48 +0500

    Layout of CUDA

--- mandel.cu
+++ mandel.cu
@@ -1,23 +1,83 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-#define img_w 1920
-#define img_h 1080
-#define channels 3
+#define IMG_W 1920
+#define IMG_H 1080
+#define CHANNELS 3
+#define MAX_ITR 100
+#define REAL_MIN -2.0
+#define REAL_MAX 1.0
+#define IMAG_MIN -1.5
+#define IMAG_MAX 1.5
+#define INC_REAL (REAL_MAX - REAL_MIN) / IMG_W
+#define INC_IMAG (IMAG_MAX - IMAG_MIN) / IMG_H
+
+typedef struct complexNumber
+{
+    float real;
+    float imag;
+} C;
+
+float complexAbs(C *c)
+{
+    return sqrt((c->real * c->real) + (c->imag * c->imag));
+}
+
+void complexAdd(C *z, C *cnst, C *res)
+{
+    res->real = z->real + cnst->real;
+    res->imag = z->imag + cnst->imag;
+}
+
+void complexMult(C *x, C *y, C *res)
+{
+    res->real = (x->real * y->real) - (x->imag * y->imag);
+    res->imag = (x->real * y->imag) + (x->imag * y->real);
+}
+
+int mandelbrot(C *cnst)
+{
+    C z = {0.0, 0.0};
+    C zSq;
+    for (int i = 0; i < MAX_ITR; i++)
+    {
+        if (complexAbs(&z) > 2)
+        {
+            return i;
+        }
+        complexMult(&z, &z, &zSq);
+        complexAdd(&zSq, cnst, &z);
+    }
+    return MAX_ITR;
+}
+
+void getColor(int itrs, unsigned char *r, unsigned char *g, unsigned char *b)
+{
+    *r = (unsigned char)(itrs * 2.0f);
+    *g = (unsigned char)(itrs * 1.9f);
+    *b = (unsigned char)(itrs * 2.35f);
+}
 
 int main(void){
-    // each frame contains the pixels mapped to the region's range
-    // scaling factor is to be multiplied by the region's range
-    // change the region variables by scaling amount (and point of zoom) after each iteration
-
-    // generating one frame
-    // malloc w x h matrix on host
-    // malloc w x h matric on device
-    // call kernel with scaling factor, device matrix
-    // copy matrix from kernel onto host
-    // pass host matrix to stb_image_write
-    // cudaDeviceSynchronize();
+    // figure out 2D blocks with 2D threads or 1D blocks with 2D threads
+    // I would have to interpolate blockIdx and threadIdx in x and y to find x and y pixels co-ords
+    // formula to calculate real and imag for each parallel kernel
+    // real = real_min + (x * real_inc) , x is interpolated
+    // imag = imag_min + (y * imag_inc) , y is interpolated
+    // each kernel will be launched with a zoom factor as function parameter
+    // flow:
+    // loop with scaling factor
+    // launch kernel with scaling factor
+    // inside kernel:
+    // calculate x and y by interpolating
+    // calculate real and imag
+    // run function normally
+    // kernel end
+    // cudadevice synch
+    // devicetohostcopy
+    // pass to stb_img
+    // repeat loop
 }
